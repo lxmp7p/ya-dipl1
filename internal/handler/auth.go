39c552a4fda600aaea44400d9b5a99b3fc2221f8
Zaur <you@example.com>
2026-04-28 12:49:42 +0300

    merge dev

--- internal/handler/auth.go
+++ internal/handler/auth.go
@@ -67,6 +67,7 @@ func (auth *AuthHandler) Registration(w http.ResponseWriter, r *http.Request) {
 	})
 
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Authorization", sessionID)
 
 	json.NewEncoder(w).Encode(RegisterResponse{
 		SessionID: sessionID,
