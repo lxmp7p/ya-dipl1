1c1117457fe0326c7c03e109b5c2cb52dbf00ab2
Zaur <you@example.com>
2026-04-29 15:58:13 +0300

    add put order

--- internal/handler/middlewares.go
+++ internal/handler/middlewares.go
@@ -10,12 +10,22 @@ import (
 func AuthMiddleware(auth repository.Auth) func(http.Handler) http.Handler {
 	return func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			authHeader := r.Header.Get("Authorization")
-			if authHeader == "" {
+			var sessionID string
+			sessionID = r.Header.Get("Authorization")
+
+			if sessionID == "" {
+				cookie, err := r.Cookie("session_id")
+				if err == nil && cookie != nil {
+					sessionID = cookie.Value
+				}
+			}
+
+			if sessionID == "" {
 				w.WriteHeader(http.StatusUnauthorized)
 				return
 			}
-			session, err := auth.CheckAuthDataBySession(r.Context(), authHeader)
+
+			session, err := auth.CheckAuthDataBySession(r.Context(), sessionID)
 			if err != nil {
 				w.WriteHeader(http.StatusUnauthorized)
 				return
